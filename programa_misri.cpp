5b9bf071bdbe6c9d8f3886d848c7e1d247013ce9
ainis997 <235386176+ainis997@users.noreply.github.com>
2026-02-12 23:04:06 +0200

    Beveik baigta kurti masyvinė programos versija.

--- programa_misri.cpp
+++ programa_misri.cpp
@@ -30,15 +30,15 @@ struct Studentas
     double rezas_med;
 };
 
-void ivestis(vector<Studentas> &grupe);
-void isvestis(vector<Studentas> &grupe);
+void ivestis(Studentas *&grupe, int &studentu_masyvo_dydis);
+void isvestis(Studentas *&grupe);
 
 // DEĪKTAN DABBER NI LABBAN ĒNSTAN PŪNKCIJAN
 template <typename T>
-void padid_masyva(int &dab_dydis, T *masyvas)
+void padid_masyva(int &dab_dydis, T *&masyvas)
 {
-    T *naujo_masyvo_rod = new int[dab_dydis * 2];
-    std::copy_n(masyvas, dab_dydis, naujo_masyvo_rod);
+    T *naujo_masyvo_rod = new T[dab_dydis * 2];
+    std::copy_n(masyvas, dab_dydis, naujo_masyvo_rod); // arba: std::move(..)
     dab_dydis *= 2;
     delete[] masyvas;
     masyvas = naujo_masyvo_rod;
@@ -76,9 +76,13 @@ int main()
         //
         break;
     }*/
-    vector<Studentas> grupe;
-    ivestis(grupe);
+    int studentu_masyvo_dydis = 5;
+    Studentas *grupe = new Studentas[studentu_masyvo_dydis];
+    // vector<Studentas> grupe;
+    ivestis(grupe, studentu_masyvo_dydis);
     isvestis(grupe);
+
+    delete[] grupe;
 
     // pritaikymas terminalui: tinkama programos pabaiga (kad vartotojas spėtų pamatyti išvestį)
     cin.ignore(); // išvalo įvesties buferį (atmintį); be jo — programos langas iš karto išsijungia
@@ -87,8 +91,19 @@ int main()
     cin.get(); // lauks kol vartotojas paspaus enter
 }
 
-void ivestis(vector<Studentas> &grupe)
+int studentu_sk = 0; // nustatom čia, kad būtų globalus, visur matomas (reikia jo ir išvesties fjai)
+
+void ivestis(Studentas *&grupe, int &studentu_masyvo_dydis)
 {
+    int min_iverciu_sk;
+    cout << "Iveskite, kiek studentai privalo tureti iverciu: ";
+    while (!(cin >> min_iverciu_sk) || min_iverciu_sk <= 0)
+    {
+        cout << "Netinkama ivestis. Iveskite minimalu iverciu skaiciu: ";
+        cin.clear();
+        cin.ignore(MAX_INT, '\n');
+    }
+
     for (int i = 0;; i++)
     {
         if (i > 0)
@@ -102,6 +117,11 @@ void ivestis(vector<Studentas> &grupe)
             }
             if (arDarVestiStudenta == "n")
                 break;
+        }
+
+        if (studentu_sk == studentu_masyvo_dydis)
+        {
+            padid_masyva(studentu_masyvo_dydis, grupe);
         }
 
         Studentas A;
@@ -122,26 +142,15 @@ void ivestis(vector<Studentas> &grupe)
             try
             {
                 pazymys = std::stoi(ivercio_ivestis); // std::stoi funkcija paverčia string į int.
-                if (pazymys < 1 || pazymys > 10)
+                if (pazymys < 0 || pazymys > 10)
                     throw "Netinkama ivestis"; // tuščio "throw;" negalima palikt, nes td tsg užlauš programą
                 A.pazymiai[iverciu_sk] = pazymys;
                 iverciu_suma += pazymys;
                 iverciu_sk++;
 
                 // MASYVO PADIDINIMAS (jeigu pasiekiama esama masyvo dydžio riba)
-                if (iverciu_sk >= A.masyvo_dydis)
-                {
+                if (iverciu_sk == A.masyvo_dydis)
                     padid_masyva(A.masyvo_dydis, A.pazymiai);
-                    /*int *naujo_masyvo_rod = new int[A.masyvo_dydis * 2]; // paskiriam naują, 2kart didesnę dinam. atmintį masyvui
-                    for (int i = 0; i < A.masyvo_dydis; i++)
-                    {
-                        naujo_masyvo_rod[i] = A.pazymiai[i]; // perkeliam seno masyvo elementus naujon vieton
-                    }
-                    A.masyvo_dydis *= 2;           // padidinam masyvo_dydis nario vertę (kadangi padidės masyvas)
-                    delete[] A.pazymiai;           // nuo masyvo rodyklės atlaisvinam senąją (mažesnę) masyvo atmintį
-                    A.pazymiai = naujo_masyvo_rod; // masyvo rodyklei priskiriam naują (didesnės atminties) rodyklę
-                    */
-                }
             }
             catch (...) // "..." argumentas sako, kad priimk bet kokią klaidą; šiuo catch bloku valdom dvi klaidas: string>int konvertavimo galimą klaidą IR netinkamą pažymio skaitinę vertę (ne tarp 1 ir 10)
             {
@@ -150,7 +159,7 @@ void ivestis(vector<Studentas> &grupe)
         }
 
         cout << "Iveskite egzamino vertinima: ";
-        while (!(cin >> A.egzo_rezas) || A.egzo_rezas < 1 || A.egzo_rezas > 10)
+        while (!(cin >> A.egzo_rezas) || A.egzo_rezas < 0 || A.egzo_rezas > 10)
         {
             cout << "Netinkama ivestis. Iveskite pazymi tarp 1 ir 10: ";
             cin.clear();
@@ -158,24 +167,43 @@ void ivestis(vector<Studentas> &grupe)
         }
 
         // vidurkio apsk.
-        if (iverciu_sk == 0)
-            A.rezas_vid = A.egzo_rezas * 0.6;
+        if (iverciu_sk < min_iverciu_sk)
+            A.rezas_vid = ((iverciu_suma * 1.0) / (min_iverciu_sk * 1.0)) * 0.4 + (A.egzo_rezas * 0.6);
         else
-            A.rezas_vid = (iverciu_suma * 1.0) / (iverciu_sk * 1.0) * 0.4 + (A.egzo_rezas * 0.6);
+            A.rezas_vid = ((iverciu_suma * 1.0) / (iverciu_sk * 1.0)) * 0.4 + (A.egzo_rezas * 0.6);
 
         // medianos apsk.
-        int visu_pazymiu_sk = iverciu_sk + 1;
-        int *visi_pazymiai = new int[visu_pazymiu_sk]; // + 1 — nes reiks dar pridėt egzamino pažymį
-        for (int i = 0; i < iverciu_sk; i++)
+        int *visi_pazymiai;
+        // DAR REIKIA PATIKRINT AR ČIA TIKRAI VISKAS GERAI: JEIGU NE, TAI GAL PATS SKAIČIAVIMAS ARBA FOR LOOPAS KURIS; NES TESTAVIME ATRODĖ BŠK NUKRYPĘ
+        if (iverciu_sk < min_iverciu_sk)
         {
-            visi_pazymiai[i] = A.pazymiai[i];
+            int visu_pazymiu_sk = min_iverciu_sk + 1;
+            visi_pazymiai = new int[visu_pazymiu_sk];
+            for (int i = 0; i < iverciu_sk; i++)
+                visi_pazymiai[i] = A.pazymiai[i];
+            for (int i = iverciu_sk; i < min_iverciu_sk; i++)
+                visi_pazymiai[i] = 0;
+            visi_pazymiai[min_iverciu_sk] = A.egzo_rezas;
+            std::sort(visi_pazymiai, visi_pazymiai + min_iverciu_sk); // surikiuoja masyvą did. tvarka (nuo pirmo lig paskutinio elemento)
+            if ((visu_pazymiu_sk) % 2 != 0)
+                A.rezas_med = visi_pazymiai[visu_pazymiu_sk / 2];
+            else
+                A.rezas_med = (visi_pazymiai[(visu_pazymiu_sk / 2) - 1] + visi_pazymiai[visu_pazymiu_sk / 2]) / 2.0;
         }
-        visi_pazymiai[visu_pazymiu_sk - 1] = A.egzo_rezas;
-        std::sort(visi_pazymiai, visi_pazymiai + visu_pazymiu_sk - 1); // surikiuoja masyvą did. tvarka (nuo pirmo lig paskutinio elemento)
-        if ((iverciu_sk + 1) % 2 != 0)
-            A.rezas_med = visi_pazymiai[visu_pazymiu_sk / 2];
         else
-            A.rezas_med = (visi_pazymiai[(visu_pazymiu_sk / 2) - 1] + visi_pazymiai[visu_pazymiu_sk / 2]) / 2.0;
+        {
+            int visu_pazymiu_sk = iverciu_sk + 1;
+            visi_pazymiai = new int[visu_pazymiu_sk]; // + 1 — nes reiks dar pridėt egzamino pažymį
+            for (int i = 0; i < iverciu_sk; i++)
+                visi_pazymiai[i] = A.pazymiai[i];
+            visi_pazymiai[visu_pazymiu_sk - 1] = A.egzo_rezas;
+            std::sort(visi_pazymiai, visi_pazymiai + visu_pazymiu_sk - 1); // surikiuoja masyvą did. tvarka (nuo pirmo lig paskutinio elemento)
+            if ((visu_pazymiu_sk) % 2 != 0)
+                A.rezas_med = visi_pazymiai[visu_pazymiu_sk / 2];
+            else
+                A.rezas_med = (visi_pazymiai[(visu_pazymiu_sk / 2) - 1] + visi_pazymiai[visu_pazymiu_sk / 2]) / 2.0;
+        }
+
         /*
         vector<int> visi_pazymiai = A.pazymiai;
         visi_pazymiai.push_back(A.egzo_rezas);
@@ -186,12 +214,15 @@ void ivestis(vector<Studentas> &grupe)
         else
             A.rezas_med = (visi_pazymiai[(visu_pazymiu_sk / 2) - 1] + visi_pazymiai[visu_pazymiu_sk / 2]) / 2.0;
         */
-        grupe.push_back(A);
+        grupe[studentu_sk] = A;
+        studentu_sk++;
+
+        delete[] visi_pazymiai;
         // A.pazymiai.clear(); // apsauga: isvalo pazymiu vektoriu, kad kitam kartojime vektorius butu tuscias
     }
 }
 
-void isvestis(vector<Studentas> &grupe)
+void isvestis(Studentas *&grupe)
 {
     string galutinio_pasirinkimas;
     bool ar_ivestas_tinkamas_galutinio_tipas = false;
@@ -208,6 +239,7 @@ void isvestis(vector<Studentas> &grupe)
     string pasirinktas_galutinis;
     if (galutinio_pasirinkimas == "v")
         pasirinktas_galutinis = "Vid.";
+
     else if (galutinio_pasirinkimas == "m")
         pasirinktas_galutinis = "Med.";
 
@@ -226,24 +258,29 @@ void isvestis(vector<Studentas> &grupe)
 
     if (galutinio_pasirinkimas == "v")
     {
-        for (const auto &A : grupe) // su &, const apsaugo nuo pakeitimo (jo nenorim)
+        for (int i = 0; i < studentu_sk; i++)
         {
             cout
-                << left << setw(15) << A.vardas
-                << left << setw(20) << A.pavarde
-                << setw(10) << std::fixed << std::setprecision(2) << A.rezas_vid
+                << left << setw(15) << grupe[i].vardas
+                << left << setw(20) << grupe[i].pavarde
+                << setw(10) << std::fixed << std::setprecision(2) << grupe[i].rezas_vid
                 << endl;
         }
     }
     else if (galutinio_pasirinkimas == "m")
     {
-        for (const auto &A : grupe) // su &, const apsaugo nuo pakeitimo (jo nenorim)
+        for (int i = 0; i < studentu_sk; i++)
         {
             cout
-                << left << setw(15) << A.vardas
-                << left << setw(20) << A.pavarde
-                << setw(10) << std::fixed << std::setprecision(2) << A.rezas_med
+                << left << setw(15) << grupe[i].vardas
+                << left << setw(20) << grupe[i].pavarde
+                << setw(10) << std::fixed << std::setprecision(2) << grupe[i].rezas_med
                 << endl;
         }
+    }
+
+    for (int i = 0; i < studentu_sk; i++)
+    {
+        delete[] grupe[i].pazymiai;
     }
 }
